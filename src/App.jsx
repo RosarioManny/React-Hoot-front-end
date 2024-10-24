5897737f10a4e1243d798ba6fd133d1a2207916d
Rani Bungay <ranibungay@gmail.com>
2024-10-24 10:33:34 -0400

    create createhootform

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
-import { useState, createContext } from 'react';
-import { Routes, Route } from 'react-router-dom';
-import NavBar from './components/NavBar/NavBar';
-import Landing from './components/Landing/Landing';
-import Dashboard from './components/Dashboard/Dashboard';
-import SignupForm from './components/SignupForm/SignupForm';
-import SigninForm from './components/SigninForm/SigninForm';
-import * as authService from '../src/services/authService'; // import the authservice
-import HootDetails from './components/HootDetails/HootDetails';
+import { useState, createContext } from "react";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import NavBar from "./components/NavBar/NavBar";
+import Landing from "./components/Landing/Landing";
+import Dashboard from "./components/Dashboard/Dashboard";
+import SignupForm from "./components/SignupForm/SignupForm";
+import SigninForm from "./components/SigninForm/SigninForm";
+import * as authService from "../src/services/authService"; // import the authservice
+import HootDetails from "./components/HootDetails/HootDetails";
+import HootForm from "./components/HootForm/HootForm";
 
 export const AuthedUserContext = createContext(null);
 
 const App = () => {
   const [user, setUser] = useState(authService.getUser()); // using the method from authservice
+
+  const handleAddHoot = async (hootFormData) => {
+    console.log("hootFormData", hootFormData);
+    navigate("/hoots");
+  };
 
   const handleSignout = () => {
     authService.signout();
@@ -19,18 +25,31 @@ const App = () => {
   };
 
   const handleDeleteHoot = async (hootId) => {
-    console.log('hootId', hootId);
+    console.log("hootId", hootId);
     setHoots(hoots.filter((hoot) => hoot._id !== hootId));
-    navigate('/hoots');
+    navigate("/hoots");
   };
-  
+
+  // src/components/HootForm/HootForm.jsx
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    props.handleAddHoot(formData);
+  };
+
   return (
     <>
       <AuthedUserContext.Provider value={user}>
         <NavBar user={user} handleSignout={handleSignout} />
         <Routes>
           {user ? (
-            <Route path="/" element={<Dashboard user={user} />} />
+            <>
+              <Route path="/" element={<Dashboard user={user} />} />
+              <Route
+                path="/hoots/new"
+                element={<HootForm handleAddHoot={handleAddHoot} />}
+              />
+            </>
           ) : (
             <Route path="/" element={<Landing />} />
           )}
--- src/services/hootService.js
+++ src/services/hootService.js
@@ -1,47 +1,58 @@
 const show = async (hootId) => {
-    try {
-      const res = await fetch(`${BASE_URL}/${hootId}`, {
-        headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
-      });
-      return res.json();
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  
-  const createComment = async (hootId, commentFormData) => {
-    try {
-      const res = await fetch(`${BASE_URL}/${hootId}/comments`, {
-        method: 'POST',
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem('token')}`,
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(commentFormData),
-      });
-      return res.json();
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  try {
+    const res = await fetch(`${BASE_URL}/${hootId}`, {
+      headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
+    });
+    return res.json();
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-  const deleteHoot = async (hootId) => {
-    try {
-      const res = await fetch(`${BASE_URL}/${hootId}`, {
-        method: 'DELETE',
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem('token')}`,
-        },
-      });
-      return res.json();
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  export {
-    index,
-    show,
-    create,
-    createComment,
-    deleteHoot,
-  };
+const createComment = async (hootId, commentFormData) => {
+  try {
+    const res = await fetch(`${BASE_URL}/${hootId}/comments`, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(commentFormData),
+    });
+    return res.json();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const deleteHoot = async (hootId) => {
+  try {
+    const res = await fetch(`${BASE_URL}/${hootId}`, {
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    });
+    return res.json();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const create = async (hootFormData) => {
+  try {
+    const res = await fetch(BASE_URL, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(hootFormData),
+    });
+    return res.json();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export { show, createComment, deleteHoot, create };
